270a1d19e9bbb4f21fc218e510f40d5d9bf95070
Menesth <poilonez78@yahoo.fr>
2025-01-22 23:39:20 +0100

    update

--- Preprocessing/Script.py
+++ Preprocessing/Script.py
@@ -10,6 +10,7 @@ def clinicaldf_preprocessing(clinicalpath):
     clinicaldf = pl.read_csv(clinicalpath)
     clinicaldf = clinicaldf.with_columns(clinicaldf["MONOCYTES"].cast(pl.Float64).alias("MONOCYTES"))
     clinicaldf = clinicaldf.drop(["CENTER", "CYTOGENETICS"])
+    
     for c in clinicaldf.columns:
         if clinicaldf[c].dtype==pl.Float64:
             clinicaldf = clinicaldf.with_columns((clinicaldf[c].fill_null(clinicaldf[c].median())).alias(c))
@@ -18,8 +19,9 @@ def clinicaldf_preprocessing(clinicalpath):
 
 def moleculardf_preprocessing(molecularpath):
     moleculardf = pl.read_csv(molecularpath, schema_overrides={"CHR": pl.Utf8})
-    #moleculardf = moleculardf.with_columns((moleculardf["END"] - moleculardf["START"]).cast(pl.Float64).alias("MUT_LEN"))
+    moleculardf = moleculardf.with_columns((moleculardf["END"] - moleculardf["START"]).cast(pl.Float64).alias("MUT_LEN"))
     moleculardf = moleculardf.drop(["CHR", "START", "END", "REF", "ALT", "GENE", "PROTEIN_CHANGE", "EFFECT"])
+    
     for c in moleculardf.columns:
         if moleculardf[c].dtype == pl.Float64:
             moleculardf = moleculardf.with_columns((moleculardf[c].fill_null(moleculardf[c].median())).alias(c))
@@ -34,8 +36,4 @@ def get_dataset(clinicalpath, molecularpath):
     clinicaldf = clinicaldf_preprocessing(clinicalpath)
     moleculardf = moleculardf_preprocessing(molecularpath)
     joint_df = clinicaldf.join(moleculardf, on="ID", how="left")
-    for c in joint_df.columns:
-        if joint_df[c].dtype == pl.Float64:
-            joint_df = joint_df.with_columns((joint_df[c].fill_null(joint_df[c].median())).alias(c))
-            joint_df = joint_df.with_columns((joint_df[c].fill_nan(joint_df[c].median())).alias(c))
     return joint_df
